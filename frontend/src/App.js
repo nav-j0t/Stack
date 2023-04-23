88d8cf18b22345dad54fbfa5f5b600e5f9225f02
nav-j0t <navjotsinghbedi2001@gmail.com>
2023-04-23 03:23:01 -0700

    Using Link and Routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import HomeScreen from './screens/HomeScreen.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import { Container } from 'react-bootstrap';
-import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
