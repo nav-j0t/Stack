1fd41da82de00aac8bde2a852b90fade767a5283
nav-j0t <navjotsinghbedi2001@gmail.com>
2023-05-18 20:09:35 -0700

    Custom errorHandler.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import connectDB from './config/db.js';
 
@@ -14,6 +15,12 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+//error for not found path/location
+app.use(notFound);
+
+//error nmiddleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
