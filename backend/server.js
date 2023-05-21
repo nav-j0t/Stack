25ead0c3b6875c9de33f4e5afe97687d409a3c9e
nav-j0t <navjotsinghbedi2001@gmail.com>
2023-05-21 00:09:07 -0700

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import connectDB from './config/db.js';
@@ -10,11 +11,14 @@ dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 //error for not found path/location
 app.use(notFound);
